456c953abf1d4d926c43f629f2a572b986d12503
GMyoDesarrollador23 <59290306+GMyoDesarrollador23@users.noreply.github.com>
2021-01-09 19:47:54 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var swLocation = "/06_twittor_PWA/sw.js";
 
 if (navigator.serviceWorker) {
    if (url.includes("localhost")) {
-      swLocation = "/sw.js";
+      swLocation = "../sw.js";
    }
    navigator.serviceWorker.register(swLocation);
 }
